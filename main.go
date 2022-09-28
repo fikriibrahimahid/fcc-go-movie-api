31a6a22c8c21867430f06433a8dc4645fd4ce8e4
fikriibrahimahid <fikriibrahim1999@gmail.com>
2022-09-28 15:46:37 +0700

    update on deleteMovie function

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ type Director struct {
 var movies []Movie
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
 
@@ -41,7 +41,14 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
 }
 
 func main() {
